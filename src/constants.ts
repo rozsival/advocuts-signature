5f476a53a7cd4eb91fd130c2d6615792816bd390
Vít Rozsíval <mail@vitrozsival.cz>
2021-11-25 17:50:24 +0100

    :lipstick: Use emoji favicon

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const ASSETS = 'assets';
 export const ASSETS_PREFIX = process.env.ASSETS_PREFIX ?? '';
 export const CONTAINER = 'container';
 export const MAP_URL = 'https://goo.gl/maps/9bu9NQn56kv9n9dY6';
-export const TITLE = '✍️ Advocuts Signature';
+export const TITLE = 'Advocuts Signature';
 export const WEBSITE = 'http://advocuts.cz';
 
 export const ASSETS_DIR = path.resolve(__dirname, ASSETS);
